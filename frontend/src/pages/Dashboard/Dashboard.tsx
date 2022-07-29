421317548ee3a67f6e918dd3ffbbed5e93385730
rodrigo novais <roo.novais@gmail.com>
2022-07-29 00:59:38 -0300

    feat(web): load documents from webservice

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Avatar from '../../components/Avatar';
@@ -6,15 +6,31 @@ import Badge, { Status } from '../../components/Badge';
 import Input from '../../components/Input';
 
 import { useAuth } from '../../contexts/auth';
+import api from '../../services/api';
 
 import DashboardCSS from './Dashboard.module.css';
+
+export type Document = {
+    id: number,
+    title: string,
+    sub_title: string,
+    status: keyof typeof Status,
+    parties: string[],
+    object: string
+}
 
 const Dashboard: React.FC = () => {
     const { signOut } = useAuth();
 
     const navigate = useNavigate();
 
+    const [documents, setDocuments] = useState<Document[]>([]);
     const [search, setSearch] = useState<string>('');
+
+    useEffect(() => {
+        api.get<Document[]>('documents')
+                .then(({ data }) => setDocuments(data));
+    }, [])
 
     const handleSignOut: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
         e.preventDefault();
@@ -54,40 +70,35 @@ const Dashboard: React.FC = () => {
                         </button>
                     </Input>
 
-                    <div className={DashboardCSS['item']}>
-                        <div className={DashboardCSS['title']}>
-                            <p>2021_08_31 Acordo de Confidencialidade</p>
-                            <p>Acordo de Confidencialidade</p>
-                        </div>
+                    { documents.map(item => (
+                        <div key={item.id} className={DashboardCSS['item']}>
+                            <div className={DashboardCSS['title']}>
+                                <p>{item.title}</p>
+                                <p>{item.sub_title}</p>
+                            </div>
 
-                        <div className={DashboardCSS['status']}>
                             <div className={DashboardCSS['status']}>
-                                <Badge status='arquivado' />
+                                <Badge status={item.status} />
+                            </div>
+
+                            <div className={DashboardCSS['description']}>
+                                <p><span>Objeto: </span>{item.object}</p>
+                            </div>
+
+                            <div className={DashboardCSS['members']}>
+                                <p>Partes do contrato</p>
+                                <div className={DashboardCSS['avatars']}>
+                                    {item.parties.map(name => <Avatar key={name} name={name} />)}
+                                </div>
+                            </div>
+
+                            <div className={DashboardCSS['menu']}>
+                                <button>
+                                    <img src='/assets/images/icons/three-dots.svg' />
+                                </button>
                             </div>
                         </div>
-
-                        <div className={DashboardCSS['description']}>
-                            <p><span>Objeto: </span>An element that renders when the image hasn't loaded.<br />
-                            This means whilst it's loading, or if there was an error.<br />
-                            If you notice a flash during loading, you can provide a delay Ms prop to delay its rendering so it only renders for those with slower connections.<br />
-                            For more control, use the onLoadingStatusChange handler on Avatar Image.</p>
-                        </div>
-
-                        <div className={DashboardCSS['members']}>
-                            <p>Partes do contrato</p>
-                            <div className={DashboardCSS['avatars']}>
-                                {['JS', 'MS', 'GS', 'CL', 'CL'].map(name => (
-                                    <Avatar key={name} name={name} />
-                                ))}
-                            </div>
-                        </div>
-
-                        <div className={DashboardCSS['menu']}>
-                            <button>
-                                <img src='/assets/images/icons/three-dots.svg' />
-                            </button>
-                        </div>
-                    </div>
+                    )) }
                 </div>
             </main>
         </div>
